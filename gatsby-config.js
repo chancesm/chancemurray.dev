1ddb04e77ccd411f7c757aa9fd3d008761c1dde2
Chance Murray <chance.s.murray@gmail.com>
2021-05-11 02:18:36 +0000

    more changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,40 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `ChanceMurray.dev`,
+  },
+  plugins: [
+    // Pages
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'page',
+      },
+    },
+    // Posts
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/posts`,
+        name: 'post',
+      },
+    },
+    // MDX
+    `gatsby-plugin-mdx`,
+    // Style Stuff
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/tips`,
+        name: 'tip',
+      },
+    },
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
